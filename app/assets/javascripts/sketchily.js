557e0314c1e4ff945b517b255a66d0dfdd3dfe5a
Scott Cytacki <scytacki@concord.org>
2013-08-16 17:38:57 -0400

    require individual files of svg-edit
    these will get combined and compressed by asset pipeline
    in dev mode they are not compressed so debugging is
    easier.

--- app/assets/javascripts/sketchily.js
+++ app/assets/javascripts/sketchily.js
@@ -5,6 +5,26 @@
 //= require jgraduate/jquery.jgraduate.min
 //= require spinbtn/JQuerySpinBtn.min
 //= require touch
-//= require svgedit.compiled
+
+// the following replaces svgedit.compiled:
+
+//= require contextmenu/jquery.contextMenu
+//= require browser
+//= require svgtransformlist
+//= require math
+//= require units
+//= require svgutils
+//= require sanitize
+//= require history
+//= require select
+//= require draw
+//= require path
+//= require svgcanvas
+//= require svg-editor
+//= require locale/locale
+//= require contextmenu
+
+// end of svgedit.compiled replacement
+
 //= require jquery-ui/jquery-ui-1.8.17.custom.min
 //= require jgraduate/jpicker
